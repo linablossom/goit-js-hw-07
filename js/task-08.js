5f3a6988e9640b752ed525dd365dee6c145cc2d8
Alina Akompi <alina.akompi@gmail.com>
2021-06-15 09:24:40 +0300

    add changes

--- js/task-08.js
+++ js/task-08.js
@@ -15,15 +15,11 @@ const createBoxes = (amount) => {
   const lastDiv = refs.boxEl.lastChild;
   const initialSize =
     lastDiv === null
-      ? 
-        startSize - step
-      : 
-        +lastDiv.style.width.replace("px", "");
+      ? startSize - step
+      : +lastDiv.style.width.replace("px", "");
   const divs = [];
   for (let i = 1; i <= amount; i += 1) {
-    //   создаем заданое количество дивов
     const div = document.createElement("div");
-    // размер дива определяется как изначальний размер + щаг * порядковий номер дива в групе
     const size = `${initialSize + step * i}px`;
     div.style.width = size;
     div.style.height = size;
